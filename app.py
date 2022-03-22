3078792764055b9d4ff0ae53f87f86f371d1f9ce
Manato Motoki <m.motoki.sys21@morijyobi.ac.jp>
2022-03-22 10:31:37 +0900

    円の面積計算機能追加

--- app.py
+++ app.py
@@ -8,4 +8,15 @@ def top_page():
 
     if _name_ == "_main_":
         app.run(dobug=True)
-        
+
+from flask import Flask, render_template, request
+
+@app.route("/circle_input")
+def circle_result():
+    return render_template("circle_input.html")
+
+@add.route("circle_result")
+def circle_result():
+    radius = int(request.args.get("radius"))
+    result = 3.14 * radius ** 2
+    return render_template("circle_result.html", result=result)
