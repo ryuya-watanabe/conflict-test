661e19a68e8a959ba0e85d6800e67fcc46ebe4e3
Ryuya Watanabe <r.watanabe.sys21@morijyobi.ac.jp>
2022-03-22 09:48:29 +0900

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask( _name_ )
+
+@app.route("/")
+def top_page():
+    return render_template("index.html")
+
+    if _name_ == "_main_":
+        app.run(dobug=True)
+        
